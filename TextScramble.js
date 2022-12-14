c8c2bafb7ad0b55611d09a27a8a746abb47ad99a
SINGSOFT <singsoft.sw@gmail.com>
2022-12-14 21:48:19 +0700

    d

---
+++ TextScramble.js
@@ -0,0 +1,59 @@
+export default class TextScramble {
+    constructor(el) {
+      this.el = el;
+      this.chars = "000!<>-_\\/[]{}—=+*^?#________";
+      this.update = this.update.bind(this);
+    }
+    setText(newText,characterChangeTimeStart = 40,characterChangeTimeEnd = 40) {
+      const oldText = this.el.innerText;
+      const length = Math.max(oldText.length, newText.length);
+      const promise = new Promise((resolve) => (this.resolve = resolve));
+      this.queue = [];
+      for (let i = 0; i < length; i++) {
+        const from = oldText[i] || "";
+        const to = newText[i] || "";
+        const start = Math.floor(Math.random() * characterChangeTimeStart);
+        const end = start + Math.floor(Math.random() * characterChangeTimeEnd);
+        this.queue.push({ from, to, start, end });
+      }
+      cancelAnimationFrame(this.frameRequest);
+      this.frame = 0;
+      this.update();
+      return promise;
+    }
+    stop() {
+      cancelAnimationFrame(this.frameRequest);
+      this.frame = 0;
+      this.update();
+    }
+    update(specialCharactersSpeed = 0.28) {
+      let output = "";
+      let complete = 0;
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { from, to, start, end, char } = this.queue[i];
+        if (this.frame >= end) {
+          complete++;
+          output += to;
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < specialCharactersSpeed) {
+            char = this.randomChar();
+            this.queue[i].char = char;
+          }
+          output += `<span class="dud">${char}</span>`;
+        } else {
+          output += from;
+        }
+      }
+      this.el.innerHTML = output;
+      if (complete === this.queue.length) {
+        this.resolve();
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update);
+        this.frame++;
+      }
+    }
+    randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)];
+    }
+  }
+  
--- jscript.js
+++ jscript.js
@@ -1,3 +1,4 @@
+import TextScramble from './TextScramble';
 // Settings Functions
 
 // สุ่ม ต่ำ - สูงสุด
@@ -11,100 +12,42 @@ const getRndInteger = (min, max) => {
 
 //  startSettings ค่าเรริ่มต้น
 let specialCharactersSpeed = 0.28; //ความเร็วในการเปลี่ยนอักษรพิเศษ
+let characterChangeTimeStart = 50; //ช่วงเวลาเปลี่ยนตัวอักษร ตอนเริ่ม
+let characterChangeTimeEnd = 50; //ช่วงเวลาเปลี่ยนตัวอักษร  ตอนจบ
+let speedChange = timeDelay(1); // เว้นช่วงช่องไฟระหว่างการเปลี่ยนตัว
+
+
+let runStatus = true;
+let countTime = 1;
+let maxTime = 10;
 
 // ——————————————————————————————————————————————————
 // Example
 // ——————————————————————————————————————————————————
-const phrases = [];
-// const phrases = [
-//   'สิรภพ โชติพาณิชย',
-// 'ปีวรา แสงตะวัน',
-// 'สราวลี ขจรเกียรติสกุล',
-// 'ขรรค์ ไกรรอด',
-// 'กนกพล สรพินิจ',
-// 'อภิลดา ศิริพัฒน์',
-// 'ดารินทร์ ปรีชาวงศ',
-// 'ชนนิกา อรุณฉาย',
-// 'ชลนิภา ปรีดาศิริกุล',
-// 'ติณณภพ เดชบุญ'
-// ];
+// const phrases = [];
+const phrases = [
+  'สิรภพ โชติพาณิชย',
+'ปีวรา แสงตะวัน',
+'สราวลี ขจรเกียรติสกุล',
+'ขรรค์ ไกรรอด',
+'กนกพล สรพินิจ',
+'อภิลดา ศิริพัฒน์',
+'ดารินทร์ ปรีชาวงศ',
+'ชนนิกา อรุณฉาย',
+'ชลนิภา ปรีดาศิริกุล',
+'ติณณภพ เดชบุญ'
+];
 
 // endSettings
-
-class TextScramble {
-  constructor(el) {
-    this.el = el;
-    this.chars = "000!<>-_\\/[]{}—=+*^?#________";
-    this.update = this.update.bind(this);
-  }
-  setText(newText) {
-    const oldText = this.el.innerText;
-    const length = Math.max(oldText.length, newText.length);
-    const promise = new Promise((resolve) => (this.resolve = resolve));
-    this.queue = [];
-    for (let i = 0; i < length; i++) {
-      const from = oldText[i] || "";
-      const to = newText[i] || "";
-      const start = Math.floor(Math.random() * 40);
-      const end = start + Math.floor(Math.random() * 40);
-      this.queue.push({ from, to, start, end });
-    }
-    cancelAnimationFrame(this.frameRequest);
-    this.frame = 0;
-    this.update();
-    return promise;
-  }
-  stop() {
-    cancelAnimationFrame(this.frameRequest);
-    this.frame = 0;
-    this.update();
-  }
-  update() {
-    let output = "";
-    let complete = 0;
-    for (let i = 0, n = this.queue.length; i < n; i++) {
-      let { from, to, start, end, char } = this.queue[i];
-      if (this.frame >= end) {
-        complete++;
-        output += to;
-      } else if (this.frame >= start) {
-        if (!char || Math.random() < specialCharactersSpeed) {
-          char = this.randomChar();
-          this.queue[i].char = char;
-        }
-        output += `<span class="dud">${char}</span>`;
-      } else {
-        output += from;
-      }
-    }
-    this.el.innerHTML = output;
-    if (complete === this.queue.length) {
-      this.resolve();
-    } else {
-      this.frameRequest = requestAnimationFrame(this.update);
-      this.frame++;
-    }
-  }
-  randomChar() {
-    return this.chars[Math.floor(Math.random() * this.chars.length)];
-  }
-}
 
 const el = document.querySelector(".text");
 const fx = new TextScramble(el);
 
-let runStatus = true;
 
-let countTime = 1;
-let maxTime = 10;
-
-let counter = 0;
-
-let speedChange = timeDelay(0.001); // ความเร็วในการสลับตัว
 
 const next = () => {
   let indexLenth = getRndInteger(0, phrases.length);
-  fx.setText(phrases[indexLenth]).then(() => {
+  fx.setText(phrases[indexLenth],characterChangeTimeStart,characterChangeTimeEnd).then(() => {
     if (runStatus) {
       setTimeout(next, speedChange);
     } else {
@@ -113,7 +56,6 @@ const next = () => {
     }
   });
   console.log("ลำดับสุ่มแสดง : " + indexLenth);
-  // counter = (counter + 1) % phrases.length;
 };
 
 const startRandom = (textID, btnID) => {
@@ -135,5 +77,5 @@ const stopRandom = () => {
   runStatus = false;
   let resut = getRndInteger(0, phrases.length); // คำตอบ
 
-  fx.setText(phrases[resut]);
+  fx.setText(phrases[resut],600,500);
 };
